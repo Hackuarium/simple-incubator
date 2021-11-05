e2e1d5e75ff92760493440746e934a3e43960b5f
andrescvargasr <andresc.vargasr@gmail.com>
2021-11-05 09:58:47 +0100

    perf: BioParams.h

--- PlatformIO/simple-incubator/include/hack/BioParams.h
+++ PlatformIO/simple-incubator/include/hack/BioParams.h
@@ -19,16 +19,16 @@
 *******************************/
 
 // #define THR_WIRE_MASTER 1
-#define THR_SST_LOGGER  1
-#define THR_SERIAL      1
+//#define THR_SST_LOGGER  1
+//#define THR_SERIAL      1
 #define THR_ONEWIRE     1
 #define THR_PID         1
 #define THR_FAN         1
 #define THR_ERROR       1
-#define THR_STEPPER     1
+//#define THR_STEPPER     1
 #define THR_STEPS       1
-#define THR_WEIGHT      1
-#define THR_OUTPUTS     1
+//#define THR_WEIGHT      1
+//#define THR_OUTPUTS     1
 #define THR_MONITORING  1
 
 
@@ -58,36 +58,24 @@
 #define EVENT_PARAMETER_SET          256
 
 // MONITORING Thread
-#define MONITORING_LED  13
+#define MONITORING_LED  5
 
-// STEPPER Thread
-#define STEPPER_DIRECTION  8
-#define STEPPER_STEP       9
-
-// OUTPUT Thread
-#define OUT_1              10
-#define OUT_2              5
-#define OUT_3              A6
-#define OUT_4              12
 
 // FAN Thread
-#define OUT_FAN            11
+#define OUT_FAN            6
 #define TEMP_FAN_ON        3500
 
 // ERROR Thread
-#define OUT_ERROR          A4
-
-// Weght Thread
-#define WEIGHT_DATA        A1
-#define WEIGHT_CLK         A0     //need to redefine the calibration parameters and process (see "HX711")
+#define OUT_ERROR          4
 
 // ONE WIRE Thread
-#define TEMP_EXT1          7
-#define TEMP_EXT2          A5
-#define TEMP_PCB           A2
+#define TEMP_PCB           A3
+#define TEMP_EXT1          A2
+#define TEMP_EXT2          A1
+#define TEMP_EXT3          A0
 
 // PID Thread
-#define PID_CONTROL        6
+#define PID_CONTROL        7
 
 // EEPROM || SST Thread
 #define EVENT_LOGGING  1
@@ -106,24 +94,14 @@
 
 
 
-#define PARAM_TEMP_EXT1      0   // A - temperature of the solution
-#define PARAM_TEMP_EXT2      1   // B - temperature of the solution
+#define PARAM_TEMP_EXT1      0   // A - temperature of the incubator
+#define PARAM_TEMP_EXT2      1   // B - temperature of the incubator
+#define PARAM_TEMP_EXT3      2   // B - temperature of the incubator
 
-#define PARAM_TEMP_PCB       2   // C - temperature of the heating plate
-#define PARAM_TEMP_PID       3   // D - heating amount of energy
-#define PARAM_TEMP_TARGET    4   // E - target temperature of the liquid
+#define PARAM_TEMP_PCB       3   // C - temperature near to MOSFET
+#define PARAM_TEMP_PID       4   // D - heating amount of energy
+#define PARAM_TEMP_TARGET    5   // E - target temperature of the incubator
 
-#define PARAM_WEIGHT                  5  // F - in unit of the balance
-#define PARAM_WEIGHT_G                6  // G - in unit of the balance
-#define PARAM_WEIGHT_SINCE_LAST_EVENT 7  // H - last weight
-#define PARAM_WEIGHT_MIN              8  // I
-#define PARAM_WEIGHT_MAX              9  // J
-
-#define PARAM_STEPPER_SPEED        26   // AA - motor speed, in RPM
-#define PARAM_STEPPER_SECONDS      27   // AB   number of seconds before changing direction
-#define PARAM_STEPPER_WAIT         28   // AC   wait time in seconds between change of direction
-#define PARAM_WEIGHT_FACTOR          29  // AD - Weight calibration: conversion factor digital -> gr (weight=FACTOR*dig_unit)
-#define PARAM_WEIGHT_OFFSET          30  // AE - Weight calibration: digital offset value when bioreactor is empty
 #define PARAM_SEDIMENTATION_TIME     31  // AF - Number of minutes to wait without rotation before emptying
 #define PARAM_FILLED_TIME            32  //AG - Number of minutes to stay in the filled state
 
@@ -143,37 +121,21 @@
 #define FLAG_TEMP_PCB_PROBE_ERROR       0    // pcb probe failed (one wire not answering)
 #define FLAG_TEMP_EXT1_PROBE_ERROR      1   // external temperature 1 probe failed (one wire not answering)
 #define FLAG_TEMP_EXT2_PROBE_ERROR      2   // external temperature 2 probe failed (one wire not answering)
-#define FLAG_TEMP_PCB_RANGE_ERROR       3   // temperature of pcb is outside range
-#define FLAG_TEMP_EXT1_RANGE_ERROR      4   // temperature of liquid is outside range
-#define FLAG_TEMP_EXT2_RANGE_ERROR      5   // temperature of liquid is outside range
-#define FLAG_TEMP_TARGET_RANGE_ERROR    6   // target temperature is outside range
-#define MASK_TEMP_ERROR                 0b01111111  // where are the bit for temperature error
-
-#define FLAG_WEIGHT_RANGE_ERROR         7   // the weight is outside range
-#define MASK_WEIGHT_ERROR               0b10000000  // where are the bit for weight error
+#define FLAG_TEMP_EXT3_PROBE_ERROR      3   // external temperature 2 probe failed (one wire not answering)
+#define FLAG_TEMP_PCB_RANGE_ERROR       4   // temperature of pcb is outside range
+#define FLAG_TEMP_EXT1_RANGE_ERROR      5   // temperature of liquid is outside range
+#define FLAG_TEMP_EXT2_RANGE_ERROR      6   // temperature of liquid is outside range
+#define FLAG_TEMP_TARGET_RANGE_ERROR    7   // target temperature is outside range
+#define MASK_TEMP_ERROR                 0b011111111  // where are the bit for temperature error
 
 #define PARAM_ENABLED            25  // Z - enabled service (set by user)
 #define PARAM_STATUS             51  // AZ - currently active service
 
 // the following flags are defined for PARAM_STATUS and PARAM_ENABLED
 #define FLAG_PID_CONTROL         0   //0 to stop PID
-#define FLAG_STEPPER_CONTROL     1   //0 to stop engine
-#define FLAG_OUTPUT_1            2
-#define FLAG_OUTPUT_2            3
-#define FLAG_OUTPUT_3            4
-#define FLAG_OUTPUT_4            5
+#define FLAG_FAN                 1
 
 // PARAM_STATUS
-#define FLAG_FOOD_CONTROL         2
-#define FLAG_PH_CONTROL           3
-#define FLAG_GAS_CONTROL          4
-#define FLAG_SEDIMENTATION        5
-#define FLAG_RELAY_FILLING        6
-#define FLAG_RELAY_EMPTYING       7
-#define FLAG_PH_CALIBRATE         8
-#define FLAG_RELAY_ACID           9
-#define FLAG_RELAY_BASE           10
-#define FLAG_WAITING_TIME_HOURS   11
 
 
 // value that should not be taken into account
